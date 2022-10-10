5e40fedb22ea3fc7c80aad936abd6a59afd28989
baymax44 <smartbay2021@outlook.com>
2022-10-10 01:08:18 -0400

    fix: implement CRUD actions

--- App.js
+++ App.js
@@ -1,31 +1,69 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
+import axios from "axios";
 
 import { Tasks, AddTask } from "./components";
+import { errHandler } from "./utils";
 
 export default function App() {
-  const [tasks, setTasks] = React.useState([
-    { id: 1, text: "Doctors Appointment", done: true },
-    { id: 2, text: "Meeting at School", done: true },
-    { id: 3, text: "Food Shopping", done: false },
-  ]);
+  const [tasks, setTasks] = React.useState([]);
 
+  // CRUD actions for RESTful API
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
+    axios
+      .delete(`http://localhost:3333/tasks/${id}`)
+      .then((res) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+      })
+      .catch(errHandler);
   };
 
-  const toggleDone = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, done: !task.done } : task
-      )
-    );
+  const toggleDone = async (id) => {
+    const task2Update = await fetchTask(id);
+
+    if (!task2Update) {
+      return;
+    }
+
+    const data = { ...task2Update, done: !task2Update.done };
+
+    axios
+      .put(`http://localhost:3333/tasks/${id}`, data)
+      .then((res) => {
+        setTasks(tasks.map((task) => (task.id === id ? res.data : task)));
+      })
+      .catch(errHandler);
   };
 
   const addTask = (text) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    setTasks([...tasks, { id, text, done: false }]);
+    axios
+      .post("http://localhost:3333/tasks", { text, done: false })
+      .then((res) => setTasks([...tasks, res.data]))
+      .catch(errHandler);
   };
+
+  const fetchTasks = () => {
+    axios
+      .get("http://localhost:3333/tasks")
+      .then((res) => {
+        setTasks(res.data);
+      })
+      .catch(errHandler);
+  };
+
+  const fetchTask = async (id) => {
+    try {
+      const res = await axios.get(`http://localhost:3333/tasks/${id}`);
+      return res.data;
+    } catch (error) {
+      errHandler(error);
+      return null;
+    }
+  };
+
+  React.useEffect(() => {
+    fetchTasks();
+  }, []);
 
   return (
     <View style={styles.container}>
